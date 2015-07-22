011d41a8a73b2e1bfb14519ff720e57c3cf1d402
dmakul <makuldauren@gmail.com>
2015-07-22 12:32:11 +0600

    123

--- assets/js/main.js
+++ assets/js/main.js
@@ -160,7 +160,7 @@
 								$submit.disabled = false;
 
 							// Show message.
-								$message._show('success', 'Thank you!');
+								$message._show('success', 'Спасибо');
 								//$message._show('failure', 'Something went wrong. Please try again.');
 
 						}, 750);
